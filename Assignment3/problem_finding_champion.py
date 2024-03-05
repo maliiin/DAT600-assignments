3579b928a3aa16ed9efc08a1559bcf3deb841e88
Gemblacensis <thomasfredhallholguin@gmail.com>
2024-03-05 11:43:54 +0100

    DFS

---
+++ Assignment3/problem_finding_champion.py
@@ -0,0 +1,50 @@
+def DFS(graph, start_node):
+    """
+    This function takes in a graph and a start node and returns a list 
+    of all the nodes that can be reached from the start node
+
+    Args: 
+        graph (dict): A dictionary where the keys are the nodes and the values 
+                      are lists of the nodes that the key node is connected to
+        start_node (str): The node to start the search from
+    
+    Returns: True if all nodes can be reached from the start node, False otherwise
+    """
+    visited = {}
+    for node in graph:
+        visited[node] = False
+    stack = [start_node]
+
+    while stack:
+        current_node = stack.pop()
+        if not visited[current_node]:
+            visited[current_node] = True
+
+        neighbors_of_current_node = graph[current_node]
+
+        # For each neighbor that hasn't been visited, add it to the stack
+        for neighbor in neighbors_of_current_node:
+            if not visited[neighbor]:
+                stack.append(neighbor)
+    #all(visited.values())
+    return visited
+
+
+def defeat_each_other(graph):
+    nodes_visited = []
+    for node in graph:
+        values = DFS(graph, node)
+        nodes_visited.append(values)
+
+if __name__ == "__main__":
+    graph = {
+        "A": ["B", "D"],
+        "B": ["A", "C"],
+        "C": ["E", "F"],
+        "D": ["B", "C"],
+        "E": ["G"],
+        "F": ["E"],
+        "G": ["F"]
+
+    }
+    defeat_each_other(graph)
